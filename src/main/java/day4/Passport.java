55b6ea6b47438beea66feda55ec69f0a52e60a58
mechanicznapomarancza <kzawadka@gmail.com>
2020-12-23 02:50:27 +0100

    Day 4 solution

---
+++ src/main/java/day4/Passport.java
@@ -0,0 +1,198 @@
+package day4;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Passport {
+
+    private String birthYear; //byr (Birth Year)
+    private String issueYear; //iyr (Issue Year)
+    private String expirationYear; //eyr (Expiration Year)
+    private String height; //hgt (Height)
+    private String hairColor; //hcl (Hair Color)
+    private String eyeColor; //ecl (Eye Color)
+    private String passportId; //pid (Passport ID)
+    private String countryId; //cid (Country ID)
+    private boolean isPassportValid;
+    private boolean isNorthPoleCredential;
+
+    public Passport() {
+
+    }
+
+    public String getBirthYear() {
+        return birthYear;
+    }
+
+    public void setBirthYear(String birthYear) {
+        this.birthYear = birthYear;
+    }
+
+    public String getIssueYear() {
+        return issueYear;
+    }
+
+    public void setIssueYear(String issueYear) {
+        this.issueYear = issueYear;
+    }
+
+    public String getExpirationYear() {
+        return expirationYear;
+    }
+
+    public void setExpirationYear(String expirationYear) {
+        this.expirationYear = expirationYear;
+    }
+
+    public String getHeight() {
+        return height;
+    }
+
+    public void setHeight(String height) {
+        this.height = height;
+    }
+
+    public String getHairColor() {
+        return hairColor;
+    }
+
+    public void setHairColor(String hairColor) {
+        this.hairColor = hairColor;
+    }
+
+    public String getEyeColor() {
+        return eyeColor;
+    }
+
+    public void setEyeColor(String eyeColor) {
+        this.eyeColor = eyeColor;
+    }
+
+    public String getPassportId() {
+        return passportId;
+    }
+
+    public void setPassportId(String passportId) {
+        this.passportId = passportId;
+    }
+
+    public String getCountryId() {
+        return countryId;
+    }
+
+    public void setCountryId(String countryId) {
+        this.countryId = countryId;
+    }
+
+    public boolean isPassportValid() {
+        return isPassportValid;
+    }
+
+    public void setPassportValid(boolean passportValid) {
+        isPassportValid = passportValid;
+    }
+
+    public boolean isNorthPoleCredential() {
+        return isNorthPoleCredential;
+    }
+
+    public void setNorthPoleCredential(boolean northPoleCredential) {
+        isNorthPoleCredential = northPoleCredential;
+    }
+
+    public void easyValidate() {
+        if (this.getBirthYear() != null &&
+                this.getIssueYear() != null &&
+                this.getExpirationYear() != null &&
+                this.getHeight() != null &&
+                this.getHairColor() != null &&
+                this.getEyeColor() != null &&
+                this.getPassportId() != null) {
+            if (this.getCountryId() != null) {
+                this.setPassportValid(true);
+                this.setNorthPoleCredential(false);
+            } else {
+                this.setPassportValid(true);
+                this.setNorthPoleCredential(true);
+            }
+        } else {
+            this.setPassportValid(false);
+            this.setNorthPoleCredential(false);
+        }
+    }
+
+    public void extendedValidation() {
+        boolean isValid;
+        //check birthYear
+        isValid = birthYearValidation();
+        //check issueYear
+        isValid &= issueYearValidation();
+        //check expirationYear
+        isValid &= expirationYearValidation();
+        //check height
+        isValid &= heightValidation();
+        //check hairColor
+        isValid &= hairColorValidation();
+        //check eyeColor
+        isValid &= eyeColorValidation();
+        //check passportId
+        isValid &= passportIdValidation();
+        //check countryId
+        this.setPassportValid(isValid);
+        if (isValid) {
+            this.setNorthPoleCredential(this.getCountryId() == null);
+        }
+    }
+
+    private boolean birthYearValidation() {
+        if (this.getBirthYear().length() == 4) {
+            final int birthYear = Integer.parseInt(this.getBirthYear());
+            return birthYear >= 1920 && birthYear <= 2002;
+        }
+        return false;
+    }
+
+    private boolean issueYearValidation() {
+        if (this.getBirthYear().length() == 4) {
+            final int issueYear = Integer.parseInt(this.getIssueYear());
+            return issueYear >= 2010 && issueYear <= 2020;
+        }
+        return false;
+    }
+
+    private boolean expirationYearValidation() {
+        if (this.getExpirationYear().length() == 4) {
+            final int expirationYear = Integer.parseInt(this.getExpirationYear());
+            return expirationYear >= 2020 && expirationYear <= 2030;
+        }
+        return false;
+    }
+
+    private boolean heightValidation() {
+        final String height = this.getHeight();
+        if (height.contains("cm")) {
+            int value = Integer.parseInt(height.substring(0, height.length() - 2));
+            return value >= 150 && value <= 193;
+        } else if (height.contains("in")) {
+            int value = Integer.parseInt(height.substring(0, height.length() - 2));
+            return value >= 59 && value <= 76;
+        }
+        return false;
+
+
+    }
+
+    private boolean hairColorValidation() {
+        return this.getHairColor().matches("^#(?:[0-9a-fA-F]{3}){1,2}$");
+    }
+
+    private boolean eyeColorValidation() {
+        List<String> possible = Arrays.asList("amb", "blu", "brn", "gry", "grn", "hzl", "oth");
+        return possible.contains(this.getEyeColor());
+
+    }
+
+    private boolean passportIdValidation() {
+        return this.getPassportId().matches("^[0-9]{9}$");
+    }
+}
---
+++ src/main/java/day4/day4.java
@@ -0,0 +1,127 @@
+package day4;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.*;
+
+public class day4 {
+
+    public static final String path = "src/main/resources/input_day4";
+
+    public static void main(String[] args) {
+        part1();
+        part2();
+    }
+
+    private static void part2() {
+        System.out.println("------ PART 2 ------");
+        List<String> input = getInput();
+        List<Passport> passports = getPassports(input);
+
+        //Extended validation
+        for (Passport passport : passports) {
+            passport.easyValidate(); //to check nulls
+            if (passport.isPassportValid()) {
+                passport.extendedValidation();
+            }
+        }
+
+        long count = passports.stream()
+                .filter(passport -> passport.isPassportValid())
+                .count();
+        System.out.println("Answer: " + count);
+    }
+
+    private static void part1() {
+        System.out.println("------ PART 1 ------");
+        List<String> input = getInput();
+        List<Passport> passports = getPassports(input);
+
+        //EasyValidation
+        for (Passport passport : passports) {
+            passport.easyValidate();
+        }
+
+        long count = passports.stream()
+                .filter(passport -> passport.isPassportValid())
+                .count();
+        System.out.println("Answer: " + count);
+    }
+
+    private static List<Passport> getPassports(List<String> input) {
+        List<Passport> possiblePassports = new ArrayList<>();
+        //Get all passports
+        List<String> parameters = new ArrayList<>();
+        for (int i = 0; i < input.size(); i++) {
+            if (!input.get(i).equals("")) {
+                final String[] params = input.get(i).split(" ");
+                parameters.addAll(Arrays.asList(params));
+            } else {
+                Passport newPassport = createPassport(parameters);
+                possiblePassports.add(newPassport);
+                parameters.clear();
+            }
+            if (i == input.size() - 1) {
+                //because there is no newline in the end
+                Passport newPassport = createPassport(parameters);
+                possiblePassports.add(newPassport);
+            }
+        }
+        return possiblePassports;
+    }
+
+
+    private static Passport createPassport(List<String> params) {
+        Passport tmpPassport = new Passport();
+        for (String param : params) {
+            final String[] paramValue = param.split(":");
+            final String key = paramValue[0];
+            final String value = paramValue[1];
+            switch (key) {
+                case "byr":
+                    tmpPassport.setBirthYear(value);
+                    break;
+                case "iyr":
+                    tmpPassport.setIssueYear(value);
+                    break;
+                case "eyr":
+                    tmpPassport.setExpirationYear(value);
+                    break;
+                case "hgt":
+                    tmpPassport.setHeight(value);
+                    break;
+                case "hcl":
+                    tmpPassport.setHairColor(value);
+                    break;
+                case "ecl":
+                    tmpPassport.setEyeColor(value);
+                    break;
+                case "pid":
+                    tmpPassport.setPassportId(value);
+                    break;
+                case "cid":
+                    tmpPassport.setCountryId(value);
+                    break;
+            }
+        }
+        return tmpPassport;
+    }
+
+
+    public static List<String> getInput() {
+        Path filePath = Paths.get(path);
+        List<String> input = new ArrayList<>();
+        Scanner scanner;
+        try {
+            scanner = new Scanner(filePath).useDelimiter("\n");
+            while (scanner.hasNext()) {
+                input.add(scanner.next());
+            }
+            scanner.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return Optional.of(input).orElse(Collections.emptyList());
+    }
+}
