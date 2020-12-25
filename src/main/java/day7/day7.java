2fe19c0aecd4b62a1513184395c5579e3afd6f24
mechanicznapomarancza <kzawadka@gmail.com>
2020-12-25 04:56:38 +0100

    Complete day 7 solution.

--- src/main/java/day7/day7.java
+++ src/main/java/day7/day7.java
@@ -25,21 +25,6 @@ public class day7 {
         System.out.println(count);
     }
 
-    private static int doesContainShinyGold(Bag rule) {
-        if (!rule.containsOther() || rule.getColor().equals(SHINY_GOLD)) {
-            return 0;
-        } else if (rule.containsShinyGold()) {
-            return 1;
-        } else {
-            int numberContains = 0;
-            for (String contain : rule.getContains()) {
-                numberContains += doesContainShinyGold(rules.stream().filter(b -> b.getColor().equals(contain)).findAny().orElseThrow());
-            }
-            return numberContains == 0 ? 0 : 1;
-        }
-
-    }
-
     private static List<Bag> fillRules() {
         List<Bag> rules = new ArrayList<>();
         for (String s : input) {
@@ -63,6 +48,44 @@ public class day7 {
     }
 
     private static void part2() {
+        final Optional<Bag> shinyGoldOptional = rules.stream().filter(b -> b.getColor().equals(SHINY_GOLD)).findFirst();
+        shinyGoldOptional.ifPresent(bag -> System.out.println(getCapacity(bag)));
+    }
+
+    private static int doesContainShinyGold(Bag rule) {
+        if (!rule.containsOther() || rule.getColor().equals(SHINY_GOLD)) {
+            return 0;
+        } else if (rule.containsShinyGold()) {
+            return 1;
+        } else {
+            int numberContains = 0;
+            for (String contain : rule.getContains()) {
+                numberContains += doesContainShinyGold(rules.stream().
+                        filter(b -> b.getColor().equals(contain))
+                        .findAny()
+                        .orElseThrow());
+            }
+            return numberContains == 0 ? 0 : 1;
+        }
+
+    }
+
+    private static int getCapacity(Bag bag) {
+        if (bag.getContains().size() == 0) {
+            return 0;
+        } else {
+            int total = 0;
+            for (int i = 0; i < bag.getContains().size(); i++) {
+                final String contain = bag.getContains().get(i);
+                final int quantity = bag.getQuantities().get(i);
+                total += quantity * getCapacity(rules.stream()
+                        .filter(b -> b.getColor().equals(contain))
+                        .findAny()
+                        .orElseThrow()) + quantity;
+
+            }
+            return total;
+        }
     }
 
     private static List<String> getInput() {
