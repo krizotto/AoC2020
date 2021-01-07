a7b6411b835ad70a567eaa0d1338e14a38da18c0
mechanicznapomarancza <kzawadka@gmail.com>
2021-01-07 02:24:40 +0100

    Changed gitignore, small refactor in Input.java

--- src/main/java/utils/Input.java
+++ src/main/java/utils/Input.java
@@ -1,13 +1,11 @@
 package utils;
 
-
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.*;
 
 public class Input {
-
 
     public static List<String> getCharInput(String path) {
 
